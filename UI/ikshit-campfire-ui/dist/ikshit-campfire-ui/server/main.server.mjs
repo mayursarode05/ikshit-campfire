9ce970e20879cc4b75e921717796521be91675ce
MayurSarode <mayursarode333@gmail.com>
2024-10-12 15:06:28 +0530

    Add Dist

---
+++ UI/ikshit-campfire-ui/dist/ikshit-campfire-ui/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-FSQWBBJR.mjs";import"./chunk-NWKEA7AY.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- UI/ikshit-campfire-ui/src/app/app.routes.ts
+++ UI/ikshit-campfire-ui/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './shared/components/index/index.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { AboutUsComponent } from './shared/components/about-us/about-us.component';
+import { ContactUsComponent } from './shared/components/contact-us/contact-us.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'index', pathMatch: 'full' },
     { path: 'index', component:IndexComponent },
-    { path: '**', redirectTo: 'index' }
+    { path: 'contactUs', component:ContactUsComponent },
+    { path: 'aboutUs', component:AboutUsComponent },
+    { path: "**", component: PageNotFoundComponent, pathMatch: 'full' }
 ];
