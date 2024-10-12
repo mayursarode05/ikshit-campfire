48d04a56d3943a7f62fce61721096a54a6cc744b
MayurSarode <mayursarode333@gmail.com>
2024-10-12 11:47:49 +0530

    initial commit

--- UI/ikshit-campfire-ui/src/app/app.routes.ts
+++ UI/ikshit-campfire-ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './shared/components/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'index', pathMatch: 'full' },
+    { path: 'index', component:IndexComponent },
+    { path: '**', redirectTo: 'index' }
+];
